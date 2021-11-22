abacc135737b3059107c9113487a43ec0179acd3
RGNRK80 <armhound@gmail.com>
2021-11-22 16:41:14 +0300

    рабочая кодировка 2 метода чтения + работают года

--- src/by/Main.java
+++ src/by/Main.java
@@ -3,6 +3,10 @@ package by;
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeSet;
 
 
 //предусмотреть создание каталога с файлом
@@ -49,14 +53,82 @@ public class Main {
         // 2  чтения текстовой информации, сразу читает текст, в отличие от потока
 
 
+        ArrayList<String>  arrtext = new ArrayList<>();  // имеем полный список слов
+        ArrayList<String>  arrYear = new ArrayList<>();  // имеем полный список дат
         FileReader reader2 = new FileReader("f:\\java\\war\\war.txt");
         StringBuilder builder = new StringBuilder();
         int c;
+
+        int counter=0;
         while ((c = reader2.read()) != -1) {
-            builder.append((char) c);
+            char ch= (char) c;
+            if (Character.isLetter(ch)) {
+                builder.append((char) c);
+                counter=0;
+            } else {
+                if (counter==0) {
+                    arrtext.add(builder.toString());
+                    builder.delete(0,builder.length());
+                    }
+                    counter++;
+                             }
         }
-        System.out.println(builder.toString());
         reader2.close();
+
+        FileReader reader3 = new FileReader("f:\\java\\war\\war.txt");
+        StringBuilder builder3 = new StringBuilder();
+
+        counter=0;
+        while ((c = reader3.read()) != -1) {
+            char ch= (char) c;
+            if (Character.isDigit(ch)) {
+                builder3.append((char) c);
+                counter=0;
+            } else {
+                if (counter==0) {
+                    arrYear.add(builder3.toString());
+                    builder3.delete(0,builder3.length());
+                }
+                counter++;
+            }
+        }
+
+
+
+
+
+
+        //System.out.println(builder.toString());
+        reader3.close();
+       // arrtext.sort();                                    прописать компаратор
+
+       // System.out.println(arrtext);
+
+        HashMap<String,Integer> mapTxt = new HashMap();
+        for (String unit: arrtext) {
+            if (mapTxt.containsKey(unit)) {mapTxt.replace(unit,mapTxt.get(unit)+1);} else {mapTxt.put(unit,1);}
+
+        }
+     //   System.out.println(mapTxt);
+     //   System.out.println(mapTxt.size());
+
+     //   System.out.println(arrYear);
+        TreeSet<Integer> sortYear = new TreeSet<>();
+        for (String unit: arrYear) {
+            if (unit.length()==4 ) {
+            if (Integer.parseInt(unit)>1200) {sortYear.add(Integer.parseInt(unit));}}
+
+        }
+
+        System.out.println(sortYear);
+
+
+
+
+
+
+
+
 
 
 
