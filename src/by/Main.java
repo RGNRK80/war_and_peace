4a1375e725190f3756342db5414419d616a33f46
RGNRK80 <armhound@gmail.com>
2021-11-22 15:21:31 +0300

    рабочая кодировка

--- src/by/Main.java
+++ src/by/Main.java
@@ -2,6 +2,10 @@ package by;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+
+//предусмотреть создание каталога с файлом
 
 public class Main {
     public Main() throws IOException {
@@ -10,16 +14,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         File file = new File("f:\\java\\war\\war.txt");
-        String url = "https://vk.com/doc354539244_619259018";
+        String url = "https://vk.com/doc354539244_619263533";
         try {
            downloadUsingStream(url, file.getAbsolutePath());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        FileReader fileReader=new FileReader(file);
+
+        // 1
 
 
+        FileInputStream fileInputStream = new FileInputStream("f:\\java\\war\\war.txt");
+        System.out.printf("File size: %d bytes \n", fileInputStream.available());
+
+        InputStreamReader isr = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(isr); // буферридер опционально
+
+
+        int i = -1;
+        StringBuilder result = new StringBuilder();
+        while ((i = reader.read()) != -1) {
+            result.append((char) i);
+        }
+        fileInputStream.close();
+
+        // rezult - переменная с текстом война и мир
+
+        System.out.println(result);
+        String[] rezz= result.toString().split(" ");
+        System.out.println(rezz[5]);
 
 
 
